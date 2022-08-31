ef2af404cbc8748a274ec7d083a45e7b33f1962a
kuwa <t.kuwazaki1024@gmail.com>
2022-09-01 00:15:33 +0900

    firebase調整

--- utils/firebase/init.ts
+++ utils/firebase/init.ts
@@ -1,11 +1,11 @@
-import { initializeApp } from "firebase/app"
+import { initializeApp } from 'firebase/app'
 
 initializeApp({
-  apiKey: "AIzaSyDF8tDo0L3SIbb89eRNJlcvox2SySj0y9g",
-  authDomain: "baby-history.firebaseapp.com",
-  projectId: "baby-history",
-  storageBucket: "baby-history.appspot.com",
-  messagingSenderId: "1044710268481",
-  appId: "1:1044710268481:web:ef591eb0f41a7aec8370c0",
-  measurementId: "G-KN4SETMSGP"
+  apiKey: 'AIzaSyDF8tDo0L3SIbb89eRNJlcvox2SySj0y9g',
+  authDomain: 'baby-history.firebaseapp.com',
+  projectId: 'baby-history',
+  storageBucket: 'baby-history.appspot.com',
+  messagingSenderId: '1044710268481',
+  appId: '1:1044710268481:web:ef591eb0f41a7aec8370c0',
+  measurementId: 'G-KN4SETMSGP',
 })
