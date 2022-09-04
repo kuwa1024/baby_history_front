0ddac4ef7ea24c9dac04009d40d1684421e0b64f
kuwa <t.kuwazaki1024@gmail.com>
2022-09-05 01:02:51 +0900

    タイトル追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,6 +22,7 @@ export default function MyApp(props: MyAppProps) {
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <meta name="theme-color" content="#1976d2" />
+        <title>赤ちゃん履歴</title>
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
