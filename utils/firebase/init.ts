4176ccc9427e69e64beb6d39c991a9a60ae49a56
kuwa <t.kuwazaki1024@gmail.com>
2022-09-05 22:45:02 +0900

    noindexとアナリティクス追加

--- utils/firebase/init.ts
+++ utils/firebase/init.ts
@@ -1,6 +1,12 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-initializeApp({
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: 'AIzaSyDF8tDo0L3SIbb89eRNJlcvox2SySj0y9g',
   authDomain: 'baby-history.firebaseapp.com',
   projectId: 'baby-history',
@@ -8,4 +14,8 @@ initializeApp({
   messagingSenderId: '1044710268481',
   appId: '1:1044710268481:web:ef591eb0f41a7aec8370c0',
   measurementId: 'G-KN4SETMSGP',
-})
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
